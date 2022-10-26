63051e37d0ce838742a0b64d626f898bf57238ba
Nelerv23 <69827955+Nelerv23@users.noreply.github.com>
2022-10-26 14:54:10 -0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        img:'https://images.unsplash.com/photo-1595907936728-c3d0aa17115e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGR1ZGV8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60' ,
+        name: 'Nelson Vasquez',
+        birthday: '02/23/2005',  
+      },
+      {
+        img:'https://images.unsplash.com/photo-1596075780750-81249df16d19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1leGljYW4lMjBtYW58ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60' ,
+        name: 'Gabriel Rabago',
+        birthday: '08/15/2005',
+      },
+      {
+        img:'https://images.unsplash.com/photo-1520216436021-3c06227e7649?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' ,
+        name: 'Cameron Rydwell',
+        birthday: '10/11/2004',
+      },
+      {
+        img:'https://images.unsplash.com/photo-1615672969032-45c313ae0a2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmlraW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60' ,
+        name: 'Trevor Toland',
+        birthday: '09/16/2005',
+      },
+];
